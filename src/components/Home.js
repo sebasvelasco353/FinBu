12e9411541843f936fb1faef2aa071ca71eefe6c
Sebastian Velasco Borrero <sebasvelasco353@hotmail.com>
2018-08-11 16:25:47 -0300

    navbar and basic home created and working

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () =>
+    <div>
+        <h1>Landing Page</h1>
+    </div>
+
+export default Home;
---
+++ src/components/containers/HomeContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import Home
+import Home from '../Home';
+
+class HomeContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Home />
+        );
+    }
+}
+export default HomeContainer;
