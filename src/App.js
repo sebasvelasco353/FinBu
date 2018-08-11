650f69f2cb9aabb3b4626b79fba1cc9b5cebd4de
Sebastian Velasco Borrero <sebasvelasco353@hotmail.com>
2018-08-11 16:31:25 -0300

    cosas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+// Routes
+import * as routes from './constants/routes';
+// import navigation component
+import Navigation from './components/Navigation';
+// import components
+import SignInContainer from './components/containers/SignInContainer';
+import HomeContainer from './components/containers/HomeContainer';
+import ProfileContainer from './components/containers/ProfileContainer';
+import SignUpContainer from './components/containers/SignUpContainer';
+import PasswordForgetContainer from './components/containers/PasswordForgetContainer';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Navigation />
+          <hr />
+          <Route
+            exact path={routes.SIGN_UP}
+            component={() => <SignUpContainer />}
+          />
+          <Route
+            exact path={routes.SIGN_IN}
+            component={() => <SignInContainer />}
+          />
+          <Route
+            exact path={routes.PASSWORD_FORGET}
+            component={() => <PasswordForgetContainer />}
+          />
+          <Route
+            exact path={routes.HOME}
+            component={() => <HomeContainer />}
+          />
+          <Route
+            exact path={routes.PROFILE}
+            component={() => <ProfileContainer />}
+          />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/';
+export const PROFILE = '/profile';
+export const PASSWORD_FORGET = '/pw-forget';
